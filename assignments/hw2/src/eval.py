0b210b1b79444016e832bd79ed7d48591c5d066d
Gary Lai <garylai1990@gmail.com>
2019-02-13 12:03:22 -0500

    :bug: (hw2) fix bugs

--- assignments/hw2/src/eval.py
+++ assignments/hw2/src/eval.py
@@ -1,14 +1,15 @@
 import os
 from time import time
 
-from src.hw2 import SentimentAnalysis
+from src.hw2 import SentimentAnalyzer
 from src.util import tsv_reader
 
 if __name__ == '__main__':
     resource_dir = os.environ.get('RESOURCE')
     tst_data = tsv_reader('{}/sst.tst.tsv'.format(resource_dir))
     start = time()
-    sentiment_analyzer = SentimentAnalysis(resource_dir)
+    sentiment_analyzer = SentimentAnalyzer(resource_dir)
+    sentiment_analyzer.load(os.path.join(resource_dir, 'hw2-model'))
     score = sentiment_analyzer.evaluate(tst_data)
     end = time()
     print(score, end - start)
--- assignments/hw2/src/hw2.py
+++ assignments/hw2/src/hw2.py
@@ -14,20 +14,21 @@
 # limitations under the License.
 # ========================================================================
 import os
-import csv
-from typing import List
-from src.util import tsv_reader
+from typing import List, Tuple
+
 from elit.component import Component
 from elit.embedding import FastText
 
+from src.util import tsv_reader
+
 
 class SentimentAnalyzer(Component):
-    def __init__(self, resource_dir: str):
+    def __init__(self, resource_dir: str, embedding_file='fasttext-50-180614.bin'):
         """
         Initializes all resources and the model.
         :param resource_dir: a path to the directory where resource files are located.
         """
-        self.vsm = FastText(os.path.join(resource_dir, 'fasttext-50-180614.bin'))
+        self.vsm = FastText(os.path.join(resource_dir, embedding_file))
         # TODO: to be filled.
 
     def load(self, model_path: str, **kwargs):
@@ -57,9 +58,10 @@ class SentimentAnalyzer(Component):
         :param kwargs:
         :return:
         """
-        trn_ys, trn_xs = zip(*[y, self.vsm.emb_list(x) for y, x in trn_data])
-        dev_ys, dev_xs = zip(*[y, self.vsm.emb_list(x) for y, x in dev_data])
+        trn_ys, trn_xs = zip(*[(y, self.vsm.emb_list(x)) for y, x in trn_data])
+        dev_ys, dev_xs = zip(*[(y, self.vsm.emb_list(x)) for y, x in dev_data])
         # TODO: to be filled
+        pass
 
     def decode(self, data: List[Tuple[int, List[str]]], **kwargs) -> List[int]:
         """
@@ -80,9 +82,10 @@ class SentimentAnalyzer(Component):
         auto_labels = self.decode(data)
         total = correct = 0
         for gold, auto in zip(gold_labels, auto_labels):
-            if gold == auto: correct += 1
+            if gold == auto:
+                correct += 1
             total += 1
-        return 100.0 *correct / total
+        return 100.0 * correct / total
 
 
 if __name__ == '__main__':
@@ -93,4 +96,4 @@ if __name__ == '__main__':
     tst_data = tsv_reader('{}/sst.tst.tsv'.format(resource_dir))
     sentiment_analyzer.train(trn_data, dev_data)
     sentiment_analyzer.evaluate(tst_data)
-    sentiment_analyzer.save(os.path.join(resource_dir, 'hw2-model')
+    sentiment_analyzer.save(os.path.join(resource_dir, 'hw2-model'))
